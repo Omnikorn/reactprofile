aeae6513c3b297f7310caecce1e14158cd01dd9d
Harith Albadry <harith.albadry@gmail.com>
2021-07-20 15:26:15 +0100

    added more linkes to about me

--- src/components/pages/about/about.page.jsx
+++ src/components/pages/about/about.page.jsx
@@ -3,7 +3,8 @@ import "./about.css"
 const image = require("./AvatarMaker.png").default
 const twitter = require("./twitter.png").default
 const insta = require("./insta.png").default
-const linked = require("./mouse.png").default
+const linked = require("./206728_connection_internet_linkedin_network_social_icon.png").default
+const github =require("./git1.png").default
 
 export default function About() {
 	return (
@@ -11,7 +12,7 @@ export default function About() {
 			<div className="container">
 				<h1 className="title"> About Me</h1>
 				<div className="row" style={{marginTop:"10%"}}>
-					<div className="col"><img  src={image} alt="harith" style={{ filter:"grayscale(90%)"}}></img></div>
+					<div className="col"><img  src={image} alt="harith" style={{ filter:"grayscale(100%)"}}></img></div>
 		           <div className="col">
 		           	 <p>
 			        
@@ -22,9 +23,10 @@ export default function About() {
 							paint.
 						</p>
 						<div className="flexrow">
-							<a style={{margin:"18px"}}><img src={twitter}  height="40px"></img></a>
-							<a style={{margin:"18px"}}><img src={insta} height="40px"></img></a>
-							<a style={{margin:"18px"}}><img src={linked} height="40px"></img></a>
+							<a href="https://twitter.com/Design81uk1" target="_blank" style={{margin:"18px"}}><img src={twitter}  height="40px"></img></a>
+							<a href="https://www.instagram.com/design_81_uk/" target="_blank" style={{margin:"18px"}}><img src={insta} height="40px"></img></a>
+							<a href="https://www.linkedin.com/in/harithalbadry/" target="_blank" style={{margin:"18px"}}><img src={linked} height="40px"></img></a>
+							<a href="https://github.com/Omnikorn" target="_blank" style={{margin:"18px"}}><img src={github} height="40px"></img></a>
 
 						</div>
 		           </div>
