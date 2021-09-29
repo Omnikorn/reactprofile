0692c5dbede37b8ba678772eb0f3cc744f58342b
Harith Albadry <harith.albadry@gmail.com>
2021-09-29 14:07:56 +0100

    updating code with typerwriter effect

--- src/components/pages/home/home.page.jsx
+++ src/components/pages/home/home.page.jsx
@@ -1,86 +1,99 @@
 import React from 'react';
 import "./home.css"
 import { useState, useEffect } from 'react';
+import Typewriter from "typewriter-effect"
 
 
 
 export default function Home() {
 
-const status =useScript("./script.js")
+// const status =useScript("./script.js")
 
-function useScript(src) {
-  // Keep track of script status ("idle", "loading", "ready", "error")
-  const [status, setStatus] = useState(src ? "loading" : "idle");
-  useEffect(
-    () => {
-      // Allow falsy src value if waiting on other data needed for
-      // constructing the script URL passed to this hook.
-      if (!src) {
-        setStatus("idle");
-        return;
-      }
-      // Fetch existing script element by src
-      // It may have been added by another intance of this hook
-      let script = document.querySelector(`script[src="${src}"]`);
-      if (!script) {
-        // Create script
-        script = document.createElement("script");
-        script.src = src;
-        script.async = true;
-        script.setAttribute("data-status", "loading");
-        // Add script to document body
-        document.body.appendChild(script);
-        // Store status in attribute on script
-        // This can be read by other instances of this hook
-        const setAttributeFromEvent = (event) => {
-          script.setAttribute(
-            "data-status",
-            event.type === "load" ? "ready" : "error"
-          );
-        };
-        script.addEventListener("load", setAttributeFromEvent);
-        script.addEventListener("error", setAttributeFromEvent);
-      } else {
-        // Grab existing script status from attribute and set to state.
-        setStatus(script.getAttribute("data-status"));
-      }
-      // Script event handler to update status in state
-      // Note: Even if the script already exists we still need to add
-      // event handlers to update the state for *this* hook instance.
-      const setStateFromEvent = (event) => {
-        setStatus(event.type === "load" ? "ready" : "error");
-      };
-      // Add event listeners
-      script.addEventListener("load", setStateFromEvent);
-      script.addEventListener("error", setStateFromEvent);
-      // Remove event listeners on cleanup
-      return () => {
-        if (script) {
-          script.removeEventListener("load", setStateFromEvent);
-          script.removeEventListener("error", setStateFromEvent);
-        }
-      };
-    },
-    [src] // Only re-run effect if script src changes
-  );
-  return status;
-}
+// function useScript(src) {
+//   // Keep track of script status ("idle", "loading", "ready", "error")
+//   const [status, setStatus] = useState(src ? "loading" : "idle");
+//   useEffect(
+//     () => {
+//       // Allow falsy src value if waiting on other data needed for
+//       // constructing the script URL passed to this hook.
+//       if (!src) {
+//         setStatus("idle");
+//         return;
+//       }
+//       // Fetch existing script element by src
+//       // It may have been added by another intance of this hook
+//       let script = document.querySelector(`script[src="${src}"]`);
+//       if (!script) {
+//         // Create script
+//         script = document.createElement("script");
+//         script.src = src;
+//         script.async = true;
+//         script.setAttribute("data-status", "loading");
+//         // Add script to document body
+//         document.body.appendChild(script);
+//         // Store status in attribute on script
+//         // This can be read by other instances of this hook
+//         const setAttributeFromEvent = (event) => {
+//           script.setAttribute(
+//             "data-status",
+//             event.type === "load" ? "ready" : "error"
+//           );
+//         };
+//         script.addEventListener("load", setAttributeFromEvent);
+//         script.addEventListener("error", setAttributeFromEvent);
+//       } else {
+//         // Grab existing script status from attribute and set to state.
+//         setStatus(script.getAttribute("data-status"));
+//       }
+//       // Script event handler to update status in state
+//       // Note: Even if the script already exists we still need to add
+//       // event handlers to update the state for *this* hook instance.
+//       const setStateFromEvent = (event) => {
+//         setStatus(event.type === "load" ? "ready" : "error");
+//       };
+//       // Add event listeners
+//       script.addEventListener("load", setStateFromEvent);
+//       script.addEventListener("error", setStateFromEvent);
+//       // Remove event listeners on cleanup
+//       return () => {
+//         if (script) {
+//           script.removeEventListener("load", setStateFromEvent);
+//           script.removeEventListener("error", setStateFromEvent);
+//         }
+//       };
+//     },
+//     [src] // Only re-run effect if script src changes
+//   );
+//   return status;
+// }
 
-const [value, setValue] = useState()
-const refresh = () => {
-  setValue({})
-}
+// const [value, setValue] = useState()
+// const refresh = () => {
+//   setValue({})
+// }
 
   return (
-    <React.Fragment>
+    <React.Fragment >
       
-      <div className="console-container">
+      {/* <div className="console-container">
 			<span id="text"></span>
 			<div className="console-underscore" id="console">
 				
 			</div>
 		</div>
-		<section className="empty"></section>
+		<section className="empty"></section> */}
+    <Typewriter 
+  options={{
+    strings:['<span style="font-size: 5rem; font-family:pwperspective">Hello World </span>  ',
+    '<span style="font-size: 5rem; font-family:pwperspective">My name is Harith </span>  ',
+    '<span style="font-size: 5rem; font-family:pwperspective">Hello World </span>  '
+  
+  
+  ], 
+    autoStart: true,
+    loop: true,
+  }}
+/>
     </React.Fragment>
   );
 }
